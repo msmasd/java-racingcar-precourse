7df967c922ab0aeffcb4e5229298eb7fa0a0b81f
msm <msmasd@naver.com>
2022-04-24 23:55:53 +0900

    feat: 자동차는 전진하는데 전진 조건은 4 이상일 경우 전진한다

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -1,11 +1,14 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class Position {
     public static final int INIT_POSITION = 0;
     public static final int MIN_MOVE_VALUE = 0;
     public static final int MAX_MOVE_VALUE = 9;
+    public static final int CAN_MOVE_EQUAL_OR_GRATER_THAN = 4;
 
-    private final int position;
+    private int position;
 
     public Position() {
         position = INIT_POSITION;
@@ -17,11 +20,27 @@ public class Position {
 
     public void move(int value) {
         moveValidate(value);
+        if (value >= CAN_MOVE_EQUAL_OR_GRATER_THAN) {
+            position++;
+        }
     }
 
     private void moveValidate(int value) {
         if (value < MIN_MOVE_VALUE || value > MAX_MOVE_VALUE) {
             throw new IllegalArgumentException("전진 판단에 사용되는 값은 0-9 값만 입력받는다");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
     }
 }
--- src/test/java/racingcar/domain/PositionTest.java
+++ src/test/java/racingcar/domain/PositionTest.java
@@ -3,6 +3,7 @@ package racingcar.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class PositionTest {
@@ -19,6 +20,15 @@ class PositionTest {
         position.move(9);
         assertThatIllegalArgumentException().isThrownBy(() -> position.move(-1));
         assertThatIllegalArgumentException().isThrownBy(() -> position.move(10));
+    }
+
+    @Test
+    void 자동차는_전진하는데_전진_조건은_4_이상일_경우_전진한다() {
+        position.move(3);
+        assertThat(position).isEqualTo(new Position(0));
+
+        position.move(4);
+        assertThat(position).isEqualTo(new Position(1));
     }
 
 }
